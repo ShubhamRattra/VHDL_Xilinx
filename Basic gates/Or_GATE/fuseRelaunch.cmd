2726d17cb3677156f3112283d878261b96fcce65
Shubham Rattra <46563834+ShubhamRattra@users.noreply.github.com>
2021-03-10 11:33:46 +0530

    Add files via upload

---
+++ Basic gates/Or_GATE/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "C:/Users/sratt/Desktop/xillinx prog/Or_GATE/Or_Gate_mod_isim_beh.exe" -prj "C:/Users/sratt/Desktop/xillinx prog/Or_GATE/Or_Gate_mod_beh.prj" "work.Or_Gate_mod" 
