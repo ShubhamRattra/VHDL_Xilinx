77ac8b76b3ae81bb067653b50e95244d6126c222
Shubham Rattra <46563834+ShubhamRattra@users.noreply.github.com>
2021-03-12 22:07:51 +0530

    Add files via upload

---
+++ Basic gates/Not_GATE/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "C:/Users/sratt/Desktop/xillinx prog/Not_GATE/Not_Gate_mod_isim_beh.exe" -prj "C:/Users/sratt/Desktop/xillinx prog/Not_GATE/Not_Gate_mod_beh.prj" "work.Not_Gate_mod" 
